25cf0aa1d1528333c89c61df5790b55add4c5deb
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-14 15:33:01 -0800

    add code that grabs user input in the main js file.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 import { DoctorIndex } from './logic';
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+  });
+});
