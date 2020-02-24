a67beda2cff63100677b4e77cccb73895f7f6629
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-24 03:32:37 -0800

    add a constructor to the DoctorServices class.

---
+++ src/doctorServices.js
@@ -0,0 +1,23 @@
+export class DoctorServices {
+  constructor() {
+    this.name = "Name: "
+    this.address = "Address: "
+    this.number = "Number: "
+    this.website = "Website: "
+    this.newPatients = "Status: "
+  }
+  async getDoctorsByName(name) {   
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+}
