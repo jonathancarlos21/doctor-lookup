ee317e959efe159f26ae693e45f2be4cffab0fbf
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-24 04:31:04 -0800

    remove constructor from doctorServices.

--- src/doctorServices.js
+++ src/doctorServices.js
@@ -1,11 +1,4 @@
 export class DoctorServices {
-  constructor() {
-    this.name = "Name: "
-    this.address = "Address: "
-    this.number = "Number: "
-    this.website = "Website: "
-    this.newPatients = "Status: "
-  }
   async getDoctorsByName(name) {   
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
