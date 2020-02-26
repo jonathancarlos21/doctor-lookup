99af8832dffed1b80a482c589ccffd77d9335555
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-26 01:04:34 -0800

    attempt to add error handling for response and search.

--- src/doctorServices.js
+++ src/doctorServices.js
@@ -3,6 +3,7 @@ export class DoctorServices {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse;
+
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
       } else {
--- src/main.js
+++ src/main.js
@@ -5,24 +5,31 @@ import $ from 'jquery';
 import { DoctorServices } from './doctorServices';
 
 $(document).ready(function() {
-  $('#getDoctors').click(function() {
-    const userInput = $('#userInput').val();
-    $('#userInput').val("");
+  $("#getDoctors").click(function() {
+    const userInput = $("#userInput").val();
+    $("#userInput").val("");
     $(".showDoctors").empty();
 
     (async () => {
       let doctorsIndex = new DoctorServices();
       const response = await doctorsIndex.getDoctors(userInput);
-      getElements(response);
-    })();
+      if (!response) {
+        $(".showErrors").append("<ul>" + response.status + "<br>" + response.statusText + "</ul>");
+      } else if (response.data.length === 0) {
+        $("showErrors").text("Sorry! There are no matches that fit your search.");
+      } else {
+        getElements(response);
+      }
+  })();
     
     function getElements(response) {
       for (let i = 0; i < response.data.length; i++) {
-        $('.showDoctors').append("<ul>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</ul>");
-        $('.showDoctors').append("<ul>" + response.data[i].practices[0].visit_address.street + "</ul>");
-        $('.showDoctors').append("<ul>" + response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip + "</ul>");
-        $('.showDoctors').append("<ul>" + response.data[i].practices[0].website + "</ul>");
-        $('.showDoctors').append("<ul>" + response.data[i].practices[0].accepts_new_patients + "</ul>");
+        $(".showDoctors").append(response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>");
+        $(".showDoctors").append(response.data[i].practices[0].visit_address.street + "<br>");
+        $(".showDoctors").append(response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip + "<br>");
+        $(".showDoctors").append(response.data[i].practices[0].phones[0].number + "<br>");
+        $(".showDoctors").append(response.data[i].practices[0].website + "<br>");
+        $(".showDoctors").append(response.data[i].practices[0].accepts_new_patients + "<br>");
       }
     }
   });
